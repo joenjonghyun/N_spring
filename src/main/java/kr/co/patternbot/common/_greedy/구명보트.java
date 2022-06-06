4f0d82d03ee9101f3368b709b9ed2f93c8928e9d
joenjonghyun <jjh980814@naver.com>
2022-06-06 15:24:04 +0900

    22.6.5

--- src/main/java/kr/co/patternbot/common/_greedy/구명보트.java
+++ src/main/java/kr/co/patternbot/common/_greedy/구명보트.java
@@ -1,6 +1,21 @@
 package kr.co.patternbot.common._greedy;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+
 //그리디 내가
 public class 구명보트 {
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Solution{ }
+    @FunctionalInterface
+    public interface SolutionService {구명보트.Solution solution(구명보트.Solution s);}
+    @Test
+    void testSolution(){}
 }
